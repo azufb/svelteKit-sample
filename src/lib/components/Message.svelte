4ba0af16e030cab79d26bc19d3e5851f32d769e9
azufb <curiousazunas@gmail.com>
2025-05-18 12:49:49 +0900

    Messageコンポーネント実装

---
+++ src/lib/components/Message.svelte
@@ -0,0 +1,17 @@
+<!-- TypeScript(JavaScript) -->
+<script lang="ts">
+	let message = 'Svelte, Hello World!';
+</script>
+
+<!-- HTML -->
+<div>
+	<p class="message">{message}</p>
+</div>
+
+<!-- Styling -->
+<style>
+	.message {
+		color: #f96743;
+		font-size: 24px;
+	}
+</style>
