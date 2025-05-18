c724ed786619ca52eacf3ef8abdc03064e87451a
azufb <curiousazunas@gmail.com>
2025-05-18 12:18:55 +0900

    ボタンコンポーネント実装

---
+++ src/lib/components/Button.svelte
@@ -0,0 +1,31 @@
+<!-- TypeScript(JavaScript) -->
+<script lang="ts">
+	import { BUTTON_THEME, type ButtonProps } from './Button.types';
+
+	let props: ButtonProps = $props();
+</script>
+
+<!-- HTML -->
+<button
+	class="button"
+	class:successButton={props.theme === BUTTON_THEME.SUCCESS}
+	class:errorButton={props.theme === BUTTON_THEME.ERROR}
+>
+	{props.label}
+</button>
+
+<!-- Styling -->
+<style>
+	.button {
+		padding: 4px 8px;
+		font-weight: bold;
+	}
+
+	.successButton {
+		background-color: #497cc8;
+	}
+
+	.errorButton {
+		background-color: #ff5757;
+	}
+</style>
---
+++ src/lib/components/Button.types.ts
@@ -0,0 +1,10 @@
+export enum BUTTON_THEME {
+	SUCCESS = 'SUCCESS',
+	ERROR = 'ERROR',
+	NORMAL = 'NORMAL'
+}
+
+export interface ButtonProps {
+	label: string;
+	theme: BUTTON_THEME;
+}
